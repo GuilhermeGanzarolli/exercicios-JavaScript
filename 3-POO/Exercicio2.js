5dcf1f3bad94c23c391213ed233397ff576ae68c
Guilherme Ganzarolli <guilhermegalmeida@hotmail.com>
2024-05-16 16:36:24 -0300

    alteração no exercício 2

--- 3-POO/Exercicio2.js
+++ 3-POO/Exercicio2.js
@@ -12,8 +12,27 @@ class Pessoa{
     falarImc(){
         const imc = this.peso/(this.altura*this.altura)
         console.log(`O IMC de ${this.nome} é ${imc.toFixed(2)}`)
+        return imc
+    }
+
+    verificarImc(){
+        imc = this.falarImc;
+        if(imc <= 18.5){
+            return 'Abaixo do peso';
+        }else if(imc > 18.5 && imc <= 25){
+            return 'Saudável';
+        }else if(imc > 25 && imc <= 30){
+            return 'A cima do peso';
+        }else if(imc > 30 && imc <= 40){
+            return 'Obeso';
+        }else{
+            return 'Obesidade Grave';
+        }
     }
 }
 
-const guilherme = new Pessoa('Guilherme', 70, 1.75);
+const jose = new Pessoa('Jose', 70, 1.75);
+jose.falarImc();
+
+const guilherme = new Pessoa('Guilherme', 76, 1.83);
 guilherme.falarImc();
